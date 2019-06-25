043f417bfadf6c34cce282d14b05fa107b790034
Stepan Krapivin <xevinbox@yandex.ru>
2019-06-25 17:24:48 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "root": true,
+  "parserOptions": {
+    "sourceType": 'module',
+    "ecmaVersion": 2017,
+  },
+  "extends": [
+    'plugin:vue/recommended',
+    'airbnb-base',
+  ],
+  "plugins": [
+    'vue'
+  ],
+};
